247ed97ca387f412826a897b01d88d4bdaae5424
tomoyay <tomoyay@gmail.com>
2019-04-16 06:03:44 +0900

    delete id

--- controllers/line.js
+++ controllers/line.js
@@ -3,8 +3,7 @@ const request = require('request');
 module.exports = {
   // オウム返し
   returnMessage: (req, res) => {
-    // const CH_ACCESS_TOKEN = '622ea6a8001a2653aaf89035c277e1c9'; //Channel Access Tokenを指定
-    const CH_ACCESS_TOKEN = 'MCCA8E/dgoRwDsKVAT1B1qpzz8Jt16Kcjvasuv+oK++0+46eyBd7nD2xhI5y33GrZThpK7T8S1JxPRYN7WtdYKj6wjeZ2mmrcizIB8lxxJ9f5iT4e9zD/Dvim/gNwOE3l8rqpeeG+e7FV1WKrvdrogdB04t89/1O/w1cDnyilFU='; //Channel Access Tokenを指定
+    const CH_ACCESS_TOKEN = ''; //Channel Access Tokenを指定
     
     // Lineプラットフォームから送信されてきたイベントを取得
     let WebhookEventObject = JSON.parse(req.body).events[0];
