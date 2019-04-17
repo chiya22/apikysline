5660ffcf3f39fe8221205a7e294b373ed6fa80f6
tomoyay <tomoyay@gmail.com>
2019-04-18 05:16:21 +0900

    middlewareの位置を修正

--- routers/line.js
+++ routers/line.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/line");
+const line = require("@line/bot-sdk");
+const config = require("../config/line.config");
 
 // LINE Bot TEST
-router.post("/", controller.returnMessage);
+router.post("/",line.middleware(config),controller.returnMessage);
 // router.route('/').post(controller.returnMessage);
 
 module.exports = router;
