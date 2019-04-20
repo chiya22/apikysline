85d5c7ae533accdf83944ed5b7740a82aaf12a4b
tomoyay <tomoyay@gmail.com>
2019-04-21 05:48:42 +0900

    simple

--- routers/index.js
+++ routers/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const router = express.Router();
 // const path = require("path");
 const PORT = process.env.PORT || 5000;
 const line = require("@line/bot-sdk"); // 追加
@@ -8,14 +9,14 @@ const config = {
   channelSecret: process.env.SECRET_KEY
 };
 
-express()
-  // .use(express.static(path.join(__dirname, "public")))
-  // .set("views", path.join(__dirname, "views"))
-  // .set("view engine", "ejs")
-  // .get("/", (req, res) => res.render("pages/index"))
-  .get("/g/", (req, res) => res.json({ method: "こんにちは、getさん" }))
-  .post("/p/", (req, res) => res.json({ method: "こんにちは、postさん" }))
-  .post("/hook/", line.middleware(config), (req, res) => lineBot(req, res)) // 変更、middlewareを追加
+// express()
+// .use(express.static(path.join(__dirname, "public")))
+// .set("views", path.join(__dirname, "views"))
+// .set("view engine", "ejs")
+// .get("/", (req, res) => res.render("pages/index"))
+// .get("/g/", (req, res) => res.json({ method: "こんにちは、getさん" }))
+// .post("/p/", (req, res) => res.json({ method: "こんにちは、postさん" }))
+router.post("/line/", line.middleware(config), (req, res) => lineBot(req, res)) // 変更、middlewareを追加
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 function lineBot(req, res) {
