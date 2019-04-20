b4183c0a392f547b9e9ca7cc0ef073803b69c2b4
tomoyay <tomoyay@gmail.com>
2019-04-21 05:50:37 +0900

    test

--- routers/index.js
+++ routers/index.js
@@ -1,37 +1,10 @@
 const express = require("express");
 const router = express.Router();
-// const path = require("path");
-const PORT = process.env.PORT || 5000;
-const line = require("@line/bot-sdk"); // 追加
-// 追加
-const config = {
-  channelAccessToken: process.env.ACCESS_TOKEN,
-  channelSecret: process.env.SECRET_KEY
-};
 
-// express()
-// .use(express.static(path.join(__dirname, "public")))
-// .set("views", path.join(__dirname, "views"))
-// .set("view engine", "ejs")
-// .get("/", (req, res) => res.render("pages/index"))
-// .get("/g/", (req, res) => res.json({ method: "こんにちは、getさん" }))
-// .post("/p/", (req, res) => res.json({ method: "こんにちは、postさん" }))
-router.post("/line/", line.middleware(config), (req, res) => lineBot(req, res)) // 変更、middlewareを追加
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+// LINE Bot TEST
+router.get("/", (req,res) => {
+  res.status(200).end;
+  res.json({result:"OK"});
+});
 
-function lineBot(req, res) {
-  res.json({ test: "hook" });
-  console.log("pass"); // 追加
-}
-
-
-// const express = require("express");
-// const router = express.Router();
-
-// // LINE Bot TEST
-// router.get("/", (req,res) => {
-//   res.status(200).end;
-//   res.json({result:"OK"});
-// });
-
-// module.exports = router;
+module.exports = router;
