ce3687b2e8c148b467a57d618cfd822233644183
tomoyay <tomoyay@gmail.com>
2019-04-21 07:14:00 +0900

    user database

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "",
+  user: "kdadueuojihsqy",
+  host: "ec2-50-17-246-114.compute-1.amazonaws.com",
+  database: "dk5ivga6uiv2q",
+  password: "79447f6017eee1f0d37a4762e071f8b7a4719dcbc7ce0d9d1d0e9ce7034f4a70",
   port: 5432
 };
--- controllers/line.js
+++ controllers/line.js
@@ -1,6 +1,6 @@
 const line = require("@line/bot-sdk");
 const config = require("../config/line.config");
-// const db = require("../models/line");
+const db = require("../models/line");
 
 module.exports = {
   returnMessage: (req, res) => {
@@ -10,23 +10,19 @@ module.exports = {
     for (let i = 0; i < events.length; i++) {
       const event = events[i];
       if (event.type === "message" && event.message.type === "text") {
-        // const record = {
-        //   replyToken: event.replyToken,
-        //   userId: event.source.userId,
-        //   userName: "",
-        //   messageId: event.message.id,
-        //   message:event.message.text,
-        //   cretaed_timestamp: event.timestamp
-        // }
-        // db.create(record, (err, data) => {
-        //   if (err){
-        //     console.log(err.message);
-        //     throw new Error(err);
-        //   }
-        // })
-        // const promises = [];
-        // promises.push(echoman(event))
-        // Promise.all(promises).then(console.log("pass")).catch(e => console.log(e));
+        const record = {
+          replyToken: event.replyToken,
+          userId: event.source.userId,
+          messageId: event.message.id,
+          message:event.message.text,
+          cretaed_timestamp: event.timestamp
+        }
+        db.create(record, (err, data) => {
+          if (err){
+            console.log(err.message);
+            throw new Error(err);
+          }
+        })
         Promise.resolve(echoman(event)).catch(e=>console.log(e));
       }
     }
