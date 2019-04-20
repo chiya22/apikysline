0d81375c1ba1564940dc9dfc0005aec27086c897
tomoyay <tomoyay@gmail.com>
2019-04-21 06:11:23 +0900

    all update

--- controllers/line.js
+++ controllers/line.js
@@ -5,41 +5,39 @@ const config = require("../config/line.config");
 module.exports = {
   returnMessage: (req, res) => {
 
-    res.status(200).end();
-    console.log("pass");
-
-    // const events = req.body.events;
-    // const client = new line.Client(config);
-    // for (let i = 0; i < events.length; i++) {
-    //   const event = events[i];
-    //   if (event.type === "message" && event.message.type === "text") {
-    //     // const record = {
-    //     //   replyToken: event.replyToken,
-    //     //   userId: event.source.userId,
-    //     //   userName: "",
-    //     //   messageId: event.message.id,
-    //     //   message:event.message.text,
-    //     //   cretaed_timestamp: event.timestamp
-    //     // }
-    //     // db.create(record, (err, data) => {
-    //     //   if (err){
-    //     //     console.log(err.message);
-    //     //     throw new Error(err);
-    //     //   }
-    //     // })
-    //     const promises = [];
-    //     promises.push(echoman(event))
-    //     Promise.all(promises).then(console.log("pass")).catch(e => console.log(e));
-    //   }
-    // }
-    // async function echoman(ev) {
-    //   // const pro =  await client.getProfile(ev.source.userId);
-    //   return client.replyMessage(ev.replyToken, {
-    //     type: "text",
-    //     text: `今「${ev.message.text}」って言いました？`
-    //     // text: `${pro.displayName}さん、今「${ev.message.text}」って言いました？`
-    //   })
-    // }
+    const events = req.body.events;
+    const client = new line.Client(config);
+    for (let i = 0; i < events.length; i++) {
+      const event = events[i];
+      if (event.type === "message" && event.message.type === "text") {
+        // const record = {
+        //   replyToken: event.replyToken,
+        //   userId: event.source.userId,
+        //   userName: "",
+        //   messageId: event.message.id,
+        //   message:event.message.text,
+        //   cretaed_timestamp: event.timestamp
+        // }
+        // db.create(record, (err, data) => {
+        //   if (err){
+        //     console.log(err.message);
+        //     throw new Error(err);
+        //   }
+        // })
+        const promises = [];
+        promises.push(echoman(event))
+        Promise.all(promises).then(console.log("pass")).catch(e => console.log(e));
+        console.log("pass");
+      }
+    }
+    async function echoman(ev) {
+      // const pro =  await client.getProfile(ev.source.userId);
+      return client.replyMessage(ev.replyToken, {
+        type: "text",
+        text: `今「${ev.message.text}」って言いました？`
+        // text: `${pro.displayName}さん、今「${ev.message.text}」って言いました？`
+      })
+    }
     
   }
 };
