b5b2bc2273c7fcdcf766b153f7b7ca414c621fd6
tomoyay <tomoyay@gmail.com>
2019-04-21 06:09:50 +0900

    change contollers call function

--- routers/line.js
+++ routers/line.js
@@ -1,23 +1,16 @@
 const express = require("express");
 const router = express.Router();
-// const controller = require("../controllers/line");
+const controller = require("../controllers/line");
 const line = require("@line/bot-sdk");
-// const config = require("../config/line.config");
+const config = require("../config/line.config");
 
-const config = {
-  channelAccessToken: process.env.ACCESS_TOKEN,
-  channelSecret: process.env.SECRET_KEY
-};
-
-router.post("/", line.middleware(config), (req, res) => lineBot(req, res)); // 変更、middlewareを追加
-
-function lineBot(req, res) {
-  res.status(200).end();
-  console.log("pass"); // 追加
-}
+// const config = {
+//   channelAccessToken: process.env.ACCESS_TOKEN,
+//   channelSecret: process.env.SECRET_KEY
+// };
 
 // LINE Bot TEST
-// router.post("/", line.middleware(config), controller.returnMessage);
+router.post("/", line.middleware(config), (req,res) => controller.returnMessage(req,res));
 // router.route('/').post(controller.returnMessage);
 
 module.exports = router;
