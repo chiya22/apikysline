39fa00edbaece1dd0a1b32daa4167472267b87a0
tomoyay <tomoyay@gmail.com>
2019-09-09 14:11:46 +0900

    test sendpushmessage

--- controllers/line.js
+++ controllers/line.js
@@ -3,6 +3,16 @@ const config = require("../config/line.config");
 const db = require("../models/line");
 
 module.exports = {
+  sendPushMessage: (mes) => {
+    const client = new line.client(config);
+    Promise.resolve(sendMessage(event)).catch(e=>console.log(e));
+    async function sendMessage(mes) {
+      return client.pushMessage("yo4da10mo8", {
+        type: "text",
+        text: mes
+      })
+    }
+  },
   returnMessage: (req, res) => {
 
     const events = req.body.events;
@@ -10,20 +20,19 @@ module.exports = {
     for (let i = 0; i < events.length; i++) {
       const event = events[i];
       if (event.type === "message" && event.message.type === "text") {
-        const record = {
-          replyToken: event.replyToken,
-          userId: event.source.userId,
-          messageId: event.message.id,
-          message:event.message.text,
-        }
-        db.create(record, (err, data) => {
-          if (err){
-            console.log(err.message);
-            throw new Error(err);
-          }
-        })
+        // const record = {
+        //   replyToken: event.replyToken,
+        //   userId: event.source.userId,
+        //   messageId: event.message.id,
+        //   message:event.message.text,
+        // }
+        // db.create(record, (err, data) => {
+        //   if (err){
+        //     console.log(err.message);
+        //     throw new Error(err);
+        //   }
+        // })
         Promise.resolve(echoman(event)).catch(e=>console.log(e));
-        Promise.resolve(sendMessage(event)).catch(e=>console.log(e));
       }
     }
     async function echoman(ev) {
@@ -33,12 +42,5 @@ module.exports = {
         text: `${pro.displayName}さん、今「${ev.message.text}」って言いました？`
       })
     }
-    async function sendMessage(ev) {
-      return client.pushMessage(ev.source.userId, {
-        type: "text",
-        text: "receive message!!"
-      })
-    }
-    
   }
 };
