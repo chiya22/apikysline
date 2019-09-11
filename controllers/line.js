0b4898806b533eb9597d53a93c15ac73901cfbe7
tomoyay <tomoyay@gmail.com>
2019-09-12 06:05:55 +0900

    divide room group

--- controllers/line.js
+++ controllers/line.js
@@ -7,19 +7,19 @@ module.exports = {
   startCron: () => {
     const client = new line.Client(config);
     cron.schedule('0 0 12 * * 1-5', () => {
-      Promise.resolve(sendMessage("お昼ですよ、メールしましょ")).catch(e=>console.log(e));
+      Promise.resolve(sendMessage("お昼ですよ、メールしましょ")).catch(e => console.log(e));
     });
     cron.schedule('0 0 13 * * 1-5', () => {
-      Promise.resolve(sendMessage("午後が始まりますよ、メールしましょ")).catch(e=>console.log(e));
+      Promise.resolve(sendMessage("午後が始まりますよ、メールしましょ")).catch(e => console.log(e));
     });
     cron.schedule('0 0 16 * * 1-5', () => {
-      Promise.resolve(sendMessage("夕ご飯どうしますか？メールしましょ")).catch(e=>console.log(e));
+      Promise.resolve(sendMessage("夕ご飯どうしますか？メールしましょ")).catch(e => console.log(e));
     });
     cron.schedule('0 0 9 28-31 * 1-5', () => {
       const date = new Date();
-      const lastDate = getLastDayOfMonth(date.getFullYear(),date.getMonth())
-      if (lastDate == date.getDate()){
-        Promise.resolve(sendMessage("締資料作成しました？作成していないなら作成しましょ")).catch(e=>console.log(e));
+      const lastDate = getLastDayOfMonth(date.getFullYear(), date.getMonth())
+      if (lastDate == date.getDate()) {
+        Promise.resolve(sendMessage("締資料作成しました？作成していないなら作成しましょ")).catch(e => console.log(e));
       }
     });
     async function sendMessage(mes) {
@@ -32,7 +32,7 @@ module.exports = {
       let date = new Date(year, month + 1, 0);
       return date.getDate();
     }
-},
+  },
   returnMessage: (req, res) => {
 
     const events = req.body.events;
@@ -40,6 +40,9 @@ module.exports = {
     for (let i = 0; i < events.length; i++) {
       const event = events[i];
       if (event.type === "message" && event.message.type === "text") {
+
+        Promise.resolve(echoman(event)).catch(e => console.log(e));
+
         // const record = {
         //   replyToken: event.replyToken,
         //   userId: event.source.userId,
@@ -53,34 +56,37 @@ module.exports = {
         //   }
         // })
         if (event.message.text === "bot帰れ") {
-          client.leaveGroup(event.source.groupId)
-          .then(() => {
-              client.replyMessage(event.replyToken, {
-                type: "text",
-                text: `${event.source.groupId}から退室します。。。`
+          if (event.source.type === "group") {
+            client.replyMessage(event.replyToken, {
+              type: "text",
+              text: `${event.source.groupId}から退室します。。。`
+            });
+            client.leaveGroup(event.source.groupId)
+              .then(() => {
+              })
+              .catch((err) => {
+                console.log(err.message);
+                throw new Error(err);
               });
-          })
-          .catch((err) => {
-            console.log(err.message);
-            throw new Error(err);
-          });
-          client.leaveRoom(event.source.roomId)
-          .then(() => {
+          }
+          if (event.source.type === "room") {
             client.replyMessage(event.replyToken, {
               type: "text",
               text: `${event.source.roomId}から退室します。。。`
-            })
-            .catch((err) => {
-              console.log(err.message);
-              throw new Error(err);
             });
-          });
+            client.leaveRoom(event.source.roomId)
+              .then(() => {
+              })
+              .catch((err) => {
+                console.log(err.message);
+                throw new Error(err);
+              });
+          };
         }
-        Promise.resolve(echoman(event)).catch(e=>console.log(e));
       }
     }
     async function echoman(ev) {
-      const pro =  await client.getProfile(ev.source.userId);
+      const pro = await client.getProfile(ev.source.userId);
       return client.replyMessage(ev.replyToken, {
         type: "text",
         text: `${pro.displayName}さん、今「${ev.message.text}」って言いました？`
