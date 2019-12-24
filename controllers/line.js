13774995473b247c5197ace4dedd94166b307a50
tomoyay <tomoyay@gmail.com>
2019-12-25 06:00:34 +0900

    te

--- controllers/line.js
+++ controllers/line.js
@@ -28,7 +28,8 @@ module.exports = {
           //<div class="bday-input">以降をカット
           uranairesult = uranairesult.slice(0,uranairesult.indexOf('<div class="bday-input">'))
           console.log(uranairesult)
-          uranairesult = iconv.decode(uranairesult,"Shift_JIS")
+          uranairesult = iconv.decode(Buffer.from(uranairesult),"windows-31j")
+          console.log(uranairesult)
           Promise.resolve(sendMessage(`${uranairesult}`)).catch(e => console.log(e))
         } catch (e) {
           console.error(e)
